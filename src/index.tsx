e7ff8fde1d0f4b4ce40c9c32279d42546e862aa2
Hasnain Roopawalla <37022937+hasnainroopawalla@users.noreply.github.com>
2024-04-07 15:44:11 +0200

    Context (#2)
    * init
    
    * WIP traverse
    
    * working types
    
    * optimize

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,41 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { useConfig } from "./config";
+
+const App = () => (
+  <div>
+    <Component1 />
+    <br />
+    <Component2 />
+    <br />
+    <Component3 />
+  </div>
+);
+
+const Component1 = () => {
+  const _config = useConfig();
+  console.log("<Component1 />", _config);
+  return <span>Component 1</span>;
+};
+
+const Component2 = () => {
+  const _config = useConfig();
+  console.log("<Component2 />", _config);
+  return <span>Component 2</span>;
+};
+
+const Component3 = () => {
+  const [counter, setCounter] = React.useState(0);
+
+  const _config = useConfig();
+  const onClick = () => setCounter(counter + 1);
+
+  console.log("<Component3 />", _config);
+  return <button onClick={onClick}>Component 3</button>;
+};
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 
-root.render(<div>hi</div>);
+root.render(<App />);
